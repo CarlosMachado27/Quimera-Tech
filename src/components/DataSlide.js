c422a7247aa2d4578931c7a21c05dc6bd7ada910
Carlos Machado <carlosmachado05@hotmail.com>
2022-07-13 15:48:08 -0300

    carousel almost done

---
+++ src/components/DataSlide.js
@@ -0,0 +1,11 @@
+export const DataSlide = [
+ { image: "https://images.unsplash.com/photo-1477927849362-0fb1e7d59ee6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+ },
+
+ { image: "https://images.unsplash.com/photo-1530292172808-dd5ec705f6ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+
+ },
+ { image: "https://images.unsplash.com/photo-1498038432885-c6f3f1b912ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+
+ }
+]
